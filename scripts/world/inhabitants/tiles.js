4c1fbaa2f8af8a838da78c138762a85a9743e320
7Magic7Mike7 <michael.artner@hotmail.de>
2022-08-21 23:47:52 +0200

    commented assert to not flood the server logs for now

--- scripts/world/inhabitants/tiles.js
+++ scripts/world/inhabitants/tiles.js
@@ -157,7 +157,7 @@ class Tile extends Configurable {
     }
 
     eat(other) {
-        console.assert(this.strength >= other.strength, "wrong direction! you're not stronger than other!");
+        //console.assert(this.strength >= other.strength, "wrong direction! you're not stronger than other!");
         other._deathTime = 0;
 
         this._energy += (other.energy * this._genome.digestionRate);
