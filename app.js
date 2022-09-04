afea996928d5f363a783ee189bfa952e076119dd
7Magic7Mike7 <michael.artner@hotmail.de>
2022-09-04 11:54:29 +0200

    implemented rerouting of /login to front page /

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/login", express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
