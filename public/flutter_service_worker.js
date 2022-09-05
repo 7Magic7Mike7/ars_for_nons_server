36158fcd61d19c11de151a48c679a49a2da132a5
7Magic7Mike7 <michael.artner@hotmail.de>
2022-09-05 14:43:21 +0200

    implemented new approach to enter subpages via button

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -17,10 +17,11 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "89c6fc6720126407cd2e36ce74bcde8f",
-"/": "89c6fc6720126407cd2e36ce74bcde8f",
-"main.dart.js": "1772924a3edb08e76a542c6222ecc943",
+"index.html": "c0ec0c1b99a600c0ac69122733546add",
+"/": "c0ec0c1b99a600c0ac69122733546add",
+"main.dart.js": "7e777507421f557207b8f033765353c3",
 "manifest.json": "557d867863879694065f2a32e0503b1b",
+"script.js": "287ecd48828e86e42e922f5a72fc70a6",
 "version.json": "81c7a4829db4f89dc5ff3dfb8adf9932",
 "WEB-INF/web.xml": "ea32e33dd7819d33bcfc7ad43bef3506"
 };
---
+++ public/script.js
@@ -0,0 +1,4 @@
+
+function openSubpage(subPage) {
+    window.open("https://www.arsfornons.com/" + subPage);
+}
